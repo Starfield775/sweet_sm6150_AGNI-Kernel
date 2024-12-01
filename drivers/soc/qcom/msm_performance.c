bebd6cfaadbf6c7207f17546683987c456a3998e
Starfield775 <starfield775@gmail.com>
2024-12-01 21:49:26 +0700

    Introducing Touchboost driver(enable by default)

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -34,6 +34,8 @@
  */
 #define POLL_INT 15
 
+static int touchboost = 1;
+
 /* To handle cpufreq min/max request */
 struct cpu_status {
 	unsigned int min;
@@ -53,6 +55,29 @@ static unsigned int aggr_big_nr;
 static unsigned int aggr_top_load;
 
 /*******************************sysfs start************************************/
+static int set_touchboost(const char *buf, const struct kernel_param *kp)
+ {
+ 	int val;
+
+ 	if (sscanf(buf, "%d\n", &val) != 1)
+ 		return -EINVAL;
+
+ 	touchboost = val;
+
+ 	return 0;
+ }
+
+ static int get_touchboost(char *buf, const struct kernel_param *kp)
+ {
+ 	return snprintf(buf, PAGE_SIZE, "%d", touchboost);
+ }
+
+ static const struct kernel_param_ops param_ops_touchboost = {
+ 	.set = set_touchboost,
+ 	.get = get_touchboost,
+ };
+ device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
+
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 {
 	int i, j, ntokens = 0;
@@ -61,7 +86,13 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpu_status *i_cpu_stats;
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
+    
+    const char *reset = "0:0 2:0";
 
+ 	if (touchboost == 0)
+ 		cp = reset;
+ 		
+ 		
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
@@ -69,7 +100,11 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	if (!(ntokens % 2))
 		return -EINVAL;
 
-	cp = buf;
+	if (touchboost == 0)
+ 		cp = reset;
+ 	else
+ 		cp = buf;
+ 		
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
